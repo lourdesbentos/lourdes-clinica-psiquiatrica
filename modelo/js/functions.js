0a5988e4ccd4d83894b0145c8e6fd88806829cb6
lourdes <lourdes.develli@gmail.com>
2020-01-11 11:54:48 -0300

    tentando fazer o commit

---
+++ modelo/js/functions.js
@@ -0,0 +1,23 @@
+﻿function comparaParImpar(){
+	var i;
+var resultado = "";
+var numero = document.querySelector("#n1").value;
+var opcao = document.querySelector("#nip");
+var verificar = numero%2;
+if(opcao.value == 2 && verificar == 0){
+i= -2;
+while (i<numero){
+i=i+2;
+
+	resultado = resultado+i+', ';}
+	
+ ;} else
+	 if(opcao.value == 1 && verificar !== 0){
+		i=-1;
+while(i<numero){
+i= i+2;
+		resultado = resultado+i+', ';};} else
+{resultado = "escolha outro número! Este não corresponde ao tipo selecionado!";}
+document.getElementById("s").innerHTML = resultado;}
+document.querySelector("#b").addEventListener("click", ()=> {comparaParImpar()
+});
