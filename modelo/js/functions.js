a546cd0cf4219b3db855064c76756f022d5fda51
lourdes <lourdes.develli@gmail.com>
2020-01-12 22:54:38 -0300

    domingo a noite apóes alterações

--- modelo/js/functions.js
+++ modelo/js/functions.js
@@ -1,23 +1,51 @@
-﻿function comparaParImpar(){
-	var i;
+﻿function mostrarLink(){
 var resultado = "";
-var numero = document.querySelector("#n1").value;
-var opcao = document.querySelector("#nip");
-var verificar = numero%2;
-if(opcao.value == 2 && verificar == 0){
-i= -2;
-while (i<numero){
-i=i+2;
+var opcao = document.querySelector("#per");
 
-	resultado = resultado+i+', ';}
-	
- ;} else
-	 if(opcao.value == 1 && verificar !== 0){
-		i=-1;
-while(i<numero){
-i= i+2;
-		resultado = resultado+i+', ';};} else
-{resultado = "escolha outro número! Este não corresponde ao tipo selecionado!";}
-document.getElementById("s").innerHTML = resultado;}
-document.querySelector("#b").addEventListener("click", ()=> {comparaParImpar()
+if(opcao.value == 1){
+resultado ='<a href="agendamentos.html" >Agendamento de consultas </a>'+'<a href="pacientes.html" >Nossos Pacientes   </a>';} else
+ if(opcao.value == 2){
+  resultado = '<a href="medicos.html" >Corpo Clínico </a>' + '<a href="historico.html" > Histórico de consultas </a>';}
+else 
+{resultado = '<a href="medicamentos.html" > Farmácia </a>';}
+document.getElementById("l").innerHTML ="<nav>"+resultado+"</nav>";}
+document.querySelector("#b").addEventListener("click", ()=> {mostrarLink()
 });
+
+/*function mostrarCampos(){
+var resultado = "";
+var opcao = document.querySelector("#med");
+
+	if(opcao.value == 1){
+		resultado ='<div  class="form-group">
+			<label for="n8" > Medicamentos da farmácia </label> 
+			<select name="n8" id="n8">
+			<option value="1"> Fluoxetina </option>
+			<option value="2"> Bupropiona  </option>
+			</select> 
+			</div>
+			<div class="form-group">
+			<label for="n9" > Dosagem </label> 
+			<select name="n9" id="n9">
+			<option value="1"> 10mg </option>
+			<option value="2"> 20 mg </option>
+			</select> 
+			</div>
+			<div class="form-group">
+			<label for="n10" > Quantidade de caixas </label>
+			<input name="n10" id="n10" type="number" class="form-control" />
+			</div>
+			<div class="form-group">
+			<fieldset>
+			 <legend> Status do pedido de medicamentos para a farmacia </legend>
+			   <label for="sol" > Medicamento solicitado  </label>
+			   <input type="checkbox"  id="sol" />
+			   <label for="ent">  Medicamento Entregue </label>
+			   <input type="checkbox"  id="ent"/>
+			   
+			</fieldset>
+			</div>';} else
+		  resultado = 'Medicamentos não foram solicitados!';}
+		document.getElementById("l").innerHTML ="<div>"+resultado+"</div>";}
+document.querySelector("#o").addEventListener("click", ()=> {mostrarCampos()
+});*/
